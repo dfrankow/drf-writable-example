424f099c89df10c6a5fe08561f9c7be8b8ba6923
Dan Frankowski <dfrankow@gmail.com>
2019-04-24 18:22:13 -0500

    Add test for Site

--- hello/tests.py
+++ hello/tests.py
@@ -1,19 +1,14 @@
-from django.contrib.auth.models import AnonymousUser, User
-from django.test import TestCase, RequestFactory
+from django.test import TestCase
 
-from .views import index
+from .serializers import SiteSerializer
 
 
-class SimpleTest(TestCase):
-    def setUp(self):
-        # Every test needs access to the request factory.
-        self.factory = RequestFactory()
+class SerializerTest(TestCase):
+    def test_serialize_site(self):
+        url = 'google.com'
+        serializer = SiteSerializer(data={'url': url})
+        serializer.is_valid(raise_exception=True)
+        site = serializer.save()
 
-    def test_details(self):
-        # Create an instance of a GET request.
-        request = self.factory.get("/")
-        request.user = AnonymousUser()
-
-        # Test my_view() as if it were deployed at /customer/details
-        response = index(request)
-        self.assertEqual(response.status_code, 200)
+        # Test the object has been created properly
+        self.assertEqual(url, site.url)
